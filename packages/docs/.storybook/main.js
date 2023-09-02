b338965b4010d7e792d1398cc6b4f90ba42a5ad9
Marcio Costa Rodrigues <marciodeth@hotmail.com>
2023-09-02 12:26:50 -0300

    adicionado addon de acessibilidade para o storybook

--- packages/docs/.storybook/main.js
+++ packages/docs/.storybook/main.js
@@ -19,7 +19,8 @@ const config = {
     getAbsolutePath("@storybook/addon-essentials"),
     getAbsolutePath("@storybook/addon-onboarding"),
     getAbsolutePath("@storybook/addon-interactions"),
-    getAbsolutePath("storybook-dark-mode")
+    getAbsolutePath("storybook-dark-mode"),
+    getAbsolutePath("@storybook/addon-a11y")
   ],
   framework: {
     name: getAbsolutePath("@storybook/react-vite"),
